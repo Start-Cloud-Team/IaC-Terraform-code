ba0ce5f788f94e66468d9d457bb6fb0f69973913
zkrxnp <102350084+zkrxnp@users.noreply.github.com>
2025-12-08 12:19:55 +0900

    Refactor variable declarations in codepipeline.tf

--- Codepipeline/codepipeline-varibles.tf
+++ Codepipeline/codepipeline-varibles.tf
@@ -1,9 +1,3 @@
-variable "account_id"{
-  type = string
-  default = "329984431650"
-  description = "account id"
-}
-
 variable "region"{
   type = string
   default = "ap-northeast-2"
@@ -13,4 +7,5 @@ variable "region"{
 variable "bucket"{
   type = string
   description = "bucket"
+
 }
