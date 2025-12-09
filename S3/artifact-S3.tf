5c5feab33f1b6924c15c5908819a108d332d6c18
zkrxnp <102350084+zkrxnp@users.noreply.github.com>
2025-12-09 14:56:59 +0900

    Update artifact-S3.tf

--- S3/artifact-S3.tf
+++ S3/artifact-S3.tf
@@ -1,3 +1,4 @@
+####################S3 bucket create####################
 resource "aws_s3_bucket" "iac-codepipeline-s3-bucket" {
     bucket = "iac-codepipeline-s3-bucket"
     force_destroy = true
@@ -6,6 +7,7 @@ resource "aws_s3_bucket" "iac-codepipeline-s3-bucket" {
     }
 }
 
+####################S3 bucket versioning####################
 resource "aws_s3_bucket_versioning" "iac-codepipeline-s3-bucket-versioning" {
     bucket = aws_s3_bucket.iac-codepipeline-s3-bucket.id
     
