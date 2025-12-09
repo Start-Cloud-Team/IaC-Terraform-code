d03afb9afadd63303d9df2098781c9ec917f82cd
zkrxnp <102350084+zkrxnp@users.noreply.github.com>
2025-12-09 14:57:55 +0900

    Update alb-sg.tf

--- alb/alb-sg.tf
+++ alb/alb-sg.tf
@@ -1,8 +1,10 @@
+####################alb security group create####################
 resource "aws_security_group" "iac-alb-sg" {
   vpc_id      = var.vpc_id
   name        = "iac-alb-sg" 
 }
 
+####################alb security group inboung rule create####################
 resource "aws_security_group_rule" "iac-sg-alb-inbound" {
   type              = "ingress"
   from_port         = 80
@@ -11,7 +13,9 @@ resource "aws_security_group_rule" "iac-sg-alb-inbound" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.iac-alb-sg.id
 }
- 
+
+####################alb security group outboung rule create####################
+
 resource "aws_security_group_rule" "mysgoutbound" {
   type              = "egress"
   from_port         = 0
@@ -19,4 +23,5 @@ resource "aws_security_group_rule" "mysgoutbound" {
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.iac-alb-sg.id
+
 }
